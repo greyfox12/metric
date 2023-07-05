5b8ea55e0ef1f2cc1160843042b2e5c19dbd7892
unknown <serg.lutokhin@gmail.com>
2023-07-05 21:47:12 +0300

    First increment

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -26,24 +26,24 @@ func gaugePage(res http.ResponseWriter, req *http.Request) {
 		}
 
 		aSt := strings.Split(st, "/")
-		if len(aSt) != 4 || len(aSt[2]) == 0 || aSt[1] != "gauge" {
+		if len(aSt) != 5 || len(aSt[3]) == 0 || aSt[2] != "gauge" {
 			res.WriteHeader(http.StatusNotFound)
 			return
 		}
 
-		metricVal, err := strconv.ParseFloat(aSt[3], 64)
+		metricVal, err := strconv.ParseFloat(aSt[4], 64)
 		if err != nil {
 			res.WriteHeader(http.StatusBadRequest)
 			return
 		}
 		// контроль длинны карты
-		if _, ok := MemMetric.gauge[aSt[2]]; ok == false && len(MemMetric.gauge) > LenArr {
+		if _, ok := MemMetric.gauge[aSt[3]]; ok == false && len(MemMetric.gauge) > LenArr {
 			res.WriteHeader(http.StatusBadRequest)
 			return
 		}
 		// Добавляю новую метрику
 
-		MemMetric.gauge[aSt[2]] = metricVal
+		MemMetric.gauge[aSt[3]] = metricVal
 
 		//		body += fmt.Sprintf("Длинна: %s\r\n", len(aSt))
 		//		body := fmt.Sprintf("Длинна: %s\r\n", len(MemMetric.gauge))
@@ -60,6 +60,9 @@ func counterPage(res http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
 		//		res.WriteHeader(http.StatusOK)
 		st := req.URL.Path
+		//		io.WriteString(res, req.URL.Path)
+
+		//		return
 		// Проверка корректности
 		if len(st) > 100 {
 			res.WriteHeader(http.StatusBadRequest)
@@ -67,24 +70,24 @@ func counterPage(res http.ResponseWriter, req *http.Request) {
 		}
 
 		aSt := strings.Split(st, "/")
-		if len(aSt) != 4 || len(aSt[2]) == 0 || aSt[1] != "counter" {
+		if len(aSt) != 5 || len(aSt[3]) == 0 || aSt[2] != "counter" {
 			res.WriteHeader(http.StatusNotFound)
 			return
 		}
 
-		metricVal, err := strconv.ParseInt(aSt[3], 10, 64)
+		metricVal, err := strconv.ParseInt(aSt[4], 10, 64)
 		if err != nil {
 			res.WriteHeader(http.StatusBadRequest)
 			return
 		}
 		// контроль длинны карты
-		if _, ok := MemMetric.counter[aSt[2]]; ok == false && len(MemMetric.counter) > LenArr {
+		if _, ok := MemMetric.counter[aSt[3]]; ok == false && len(MemMetric.counter) > LenArr {
 			res.WriteHeader(http.StatusBadRequest)
 			return
 		}
 		// Добавляю новую метрику
 
-		MemMetric.counter[aSt[2]] += metricVal
+		MemMetric.counter[aSt[3]] += metricVal
 
 		//		body += fmt.Sprintf("Длинна: %s\r\n", len(aSt))
 		//		body := fmt.Sprintf("Длинна: %s\r\n", len(MemMetric.counter))
@@ -99,8 +102,8 @@ func counterPage(res http.ResponseWriter, req *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc(`/gauge/`, gaugePage)
-	mux.HandleFunc(`/counter/`, counterPage)
+	mux.HandleFunc(`/update/gauge/`, gaugePage)
+	mux.HandleFunc(`/update/counter/`, counterPage)
 	MemMetric.gauge = make(map[string]float64, LenArr)
 	MemMetric.counter = make(map[string]int64, LenArr)
 	//gaugeMetric.val = m
